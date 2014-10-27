89d16a82c65901872c46dd4b8ffb1fad49ec2fbc
SebastianKG <sebastian.kg@gmail.com>
2014-10-27 00:04:30 -0700

    Added test class

---
+++ 410-yardwand/app/test/ApplicationTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.junit.Before;
+
+import controllers.Application;
+
+public class ApplicationTest {
+	private Application application;
+	
+	@Before
+	public void prepare() {
+		
+	}
+}
