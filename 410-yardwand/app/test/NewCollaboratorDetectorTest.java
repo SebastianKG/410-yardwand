9f802d42568bc94100e2b6afd302641c7612aad8
ericlee49 <ericlee49@gmail.com>
2014-11-19 22:20:15 -0800

    Updated NewCollabDetector JUnit test: errors fixed

--- 410-yardwand/app/test/NewCollaboratorDetectorTest.java
+++ 410-yardwand/app/test/NewCollaboratorDetectorTest.java
@@ -9,10 +9,10 @@ import org.junit.Test;
 import analysis.NewCollaboratorDetector;
 
 public class NewCollaboratorDetectorTest {
-	private static final String GOOD_URL = "https://github.com/SebastianKG/410-yardwand";
+	private static final String GOOD_URL = "https://api.github.com/repos/SebastianKG/410-yardwand/stats/contributors";
 	private static final String BAD_URL = "XXXXXXXXXXXXXXXX";
 	
-	@Test(expected=NullPointerException.class)
+	@Test
 	public void analysisCheck() {
 		NewCollaboratorDetector ncd= new NewCollaboratorDetector(GOOD_URL);
 		CollaboratorsActivities ca=ncd.getAnalysis();
@@ -23,6 +23,6 @@ public class NewCollaboratorDetectorTest {
 	public void noAnalysis(){
 		NewCollaboratorDetector ncd= new NewCollaboratorDetector(BAD_URL);
 		CollaboratorsActivities ca=ncd.getAnalysis();
-		assertEquals(ca.getActivities(),null); 
+		assertEquals(ca.getActivities().size(),0);
 	}
 }
